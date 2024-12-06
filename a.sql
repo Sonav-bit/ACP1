67d68bd17ae5baf49eb8850f54465952187e74fe
Sonav-bit <sigdelsonav09@gmail.com>
2024-12-06 19:25:39 +0545

    Add files via upload

---
+++ a.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS scholars(
+SNAME TEXT 
+SCLASS INTEGER,
+SID INTEGER,);
+
+INSERT INTO scholars(SNAME,SCLASS,SID)VALUES:
+('Sonav sigdel',8,21),
+('Aaron Bhandari',8,21),
+('Saksyam Karki',8,21),
+('Sugam Rai',8,21),
+('Jane Lama',8,21);
+
+SELECT * FROM  scholars
